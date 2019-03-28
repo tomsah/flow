7e2659aeaa020824ea6850742cef8c7cb97a1f63
Thomas Salah <thomas.salah@gmail.com>
2019-03-28 08:34:17 +0000

    testing the main App component

--- src/component/App.spec.js
+++ src/component/App.spec.js
@@ -1,15 +1,39 @@
 import React from 'react';
-import { shallow, mount } from 'enzyme';
+import { mount } from 'enzyme';
 import App from './App';
+import configureStore from '../configureStore'
+import { getAllProducts } from '../actions'
+import { Provider } from 'react-redux'
 
-it('renders without crashing', () => {
-  shallow(<App />);
-});
+let wrapper
+let store
 
-describe('@render', () => {
-  it('should render a main-wrapper div', () =>{
-    const wrapper = mount(<App />)
-    // expect(wrapper.contains(welcome)).toBe(true);
-    expect(wrapper.find('.main-wrapper'))
+
+describe('@render App', () => {
+  beforeEach(() => {
+    store = configureStore()
+    store.dispatch(getAllProducts())
+    wrapper = mount(
+      <Provider store={store}>
+        <App/>
+      </Provider>
+    )
+  })
+
+  it('should render a main-wrapper div and its child component', () =>{
+    expect(wrapper.find('.main-wrapper').length).toEqual(1)
+    expect(wrapper.find('.main').length).toEqual(1)
+    expect(wrapper.find('Header').length).toEqual(1)
+    expect(wrapper.find('ProductsPage').length).toEqual(1)
+  })
+
+  it('should render the shoppingBag component and not be visible', () => {
+    expect(wrapper.find('ShoppingBag').length).toEqual(1)
+    expect(wrapper.contains('ShoppingBag')).toBe(false)
+  })
+
+  it('should render the overlay div but not being visible', () => {
+    expect(wrapper.find('.overlay').length).toEqual(1)
+    expect(wrapper.contains('ShoppingBag')).toBe(false)
   })
 })
